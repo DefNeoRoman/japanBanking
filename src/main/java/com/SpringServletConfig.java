5dd194a7cd7cf5f97972a5ef73e590e9a44d1817
DefNeoRoman <mistarg852455>
2017-11-16 14:59:50 +0300

    init

---
+++ src/main/java/com/SpringServletConfig.java
@@ -0,0 +1,47 @@
+package com;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+//доступна конфигурация через application.properties
+@EnableWebMvc
+public class SpringServletConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public InternalResourceViewResolver resolver() {
+        InternalResourceViewResolver vr = new InternalResourceViewResolver();
+        vr.setPrefix("/WEB-INF/jsp/");
+        vr.setSuffix(".jsp");
+        return vr;
+    }
+
+    @Override
+    public void configureDefaultServletHandling(
+            DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+//        without this could not solve the problem with loading bootstrap
+        if (!registry.hasMappingForPattern("/webjars/**")) {
+            registry.addResourceHandler("/webjars/**").addResourceLocations(
+                    "classpath:/META-INF/resources/webjars/");
+        }
+        registry
+                .addResourceHandler("/resources/**")
+                .addResourceLocations("classpath:/");
+        // problem with resolving classpath directory
+
+            registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+
+    }
+
+}
